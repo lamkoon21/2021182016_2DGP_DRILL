5c780712ec3634193a3667e02c2da027629722e5
lamkoon21 <mezziroo@gmail.com>
2022-09-08 23:58:55 +0900

    name.py added

---
+++ 02/name.py
@@ -0,0 +1,116 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape()
+
+#배
+t.penup()
+t.goto(-270,200)
+t.pendown()
+t.right(90)
+t.forward(100)
+
+t.penup()
+t.goto(-200, 200)
+t.pendown()
+t.forward(100)
+
+t.penup()
+t.goto(-270, 150)
+t.pendown()
+t.left(90)
+t.forward(70)
+
+t.penup()
+t.goto(-270, 100)
+t.pendown()
+t.forward(70)
+
+t.penup()
+t.goto(-160, 220)
+t.pendown()
+t.right(90)
+t.forward(170)
+
+t.penup()
+t.goto(-160, 140)
+t.pendown()
+t.left(90)
+t.forward(30)
+
+t.penup()
+t.goto(-130, 220)
+t.pendown()
+t.right(90)
+t.forward(170)
+
+#준
+t.penup()
+t.goto(-80, 210)
+t.pendown()
+t.left(90)
+t.forward(120)
+
+t.penup()
+t.goto(-20, 210)
+t.pendown()
+t.right(140)
+t.forward(80)
+
+t.penup()
+t.goto(-20, 210)
+t.pendown()
+t.left(100)
+t.forward(80)
+
+t.penup()
+t.goto(-90, 140)
+t.pendown()
+t.left(40)
+t.forward(140)
+
+t.penup()
+t.goto(-20, 140)
+t.pendown()
+t.right(90)
+t.forward(30)
+
+t.penup()
+t.goto(-70, 100)
+t.pendown()
+t.forward(40)
+
+t.penup()
+t.goto(-70, 60)
+t.pendown()
+t.left(90)
+t.forward(110)
+
+#영
+t.penup()
+t.goto(130, 140)
+t.pendown()
+t.circle(40)
+
+t.penup()
+t.goto(180, 200)
+t.pendown()
+t.forward(30)
+
+t.penup()
+t.goto(180, 160)
+t.pendown()
+t.forward(30)
+
+t.penup()
+t.goto(210, 230)
+t.pendown()
+t.right(90)
+t.forward(120)
+
+t.penup()
+t.goto(125, 85)
+t.pendown()
+t.circle(35)
+
+input("Press any key to exit...")
