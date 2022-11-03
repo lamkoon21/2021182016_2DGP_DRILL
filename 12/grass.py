e47ef5dcb996fa50b8a831be51a35b473a6359f0
lamkoon21 <mezziroo@gmail.com>
2022-11-03 15:24:18 +0900

    drill 12 완성
    2021182016 배준영

---
+++ 12/grass.py
@@ -0,0 +1,14 @@
+from pico2d import *
+
+class Grass:
+    def __init__(self, x = 400, y = 30):
+        self.x, self.y = x, y
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        
+    def update(self):
+        pass
+
+
